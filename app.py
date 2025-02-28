2869fd65da05dfb4d5da89bce4e4bd156dce05be
ranjithdb <ranjithdbas@gmail.com>
2025-02-28 03:29:57 -0800

    add requirements.txt

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("hi")
+import requests
+
+def fetch_github():
+    response = requests.get("https://api.github.com")
+    if response.status_code == 200:
+        return "GitHub API is reachable!"
+    return "Failed to reach GitHub API."
+
+if __name__ == "__main__":
+    print(fetch_github())
